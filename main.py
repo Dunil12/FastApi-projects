999c9ec4fbd50d04c21c418aa4630ec4fbef350b
Damil <106549601+Dunil12@users.noreply.github.com>
2025-04-20 18:39:03 +0300

    пагинация
    Пагинация

--- main.py
+++ main.py
@@ -1,48 +1,13 @@
-from fastapi import FastAPI, Query, Body
+from fastapi import FastAPI
 import uvicorn
-from fastapi.openapi.docs import get_swagger_ui_html
 
-hotels = [
-    {'id' : 1, 'title' : 'Sochi', "name" : "sasha"},
-    {'id' : 2, 'title' : 'Dubai', "name" : "dasha"}
-]
+from hotels import router as router_hotels
+
+
 
 app = FastAPI()
 
-@app.get("/")
-def func():
-    return "HelloWorld!!!"
-
-@app.get("/hotelsss")
-def get_hotel(
-        id: int = Query(default=None, description='id hotel'),
-        title: str = Query(default=None, description='название отеля'),
-):
-    return [hotel for hotel in hotels if hotel["title"] == title]
-
-@app.post("/hotels")
-def create_hotel(
-        id: int = Body(embed=True),
-        title: str = Body(embed=True)
-):
-    global hotels
-    hotels.append({"id":id, "title":title})
-    return {"status" : "OK"}
-
-@app.patch("/hotels/{hotel_id}")
-def change_data(
-    hotel_id : int,# = Body(embed=True),
-    title: str = Body(embed=True),
-    name: str = Body(embed=True)
-):
-    global hotels
-    for hotel in hotels:
-        if hotel["id"] == hotel_id:
-            hotel["title"] = title
-            hotel["name"] = name
-            return {"status": "ok"}
-    return {"status": "none"}
-
+app.include_router(router_hotels)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.6", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.8", port=8000, reload=True)
